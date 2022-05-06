263c49879a950e3707faf3e61ec986835f9b288a
Kaua Mauricio <kauacariocaktd@gmail.com>
2022-05-06 11:09:41 -0300

    mudando link do auth service para o link do heroku

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -16,11 +16,11 @@ export class AuthService {
   ) { }
 
   entrar(userLogin: UserLogin): Observable<UserLogin> {     //COM Observable EU GARANTO QUE MEU ENDPOINT VAI RECEBER UM OBJETO DO TIPO UserLogin
-    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
+    return this.http.post<UserLogin>('http://app-kaua.herokuapp.com/usuarios/logar', userLogin)
   }
 
   cadastrar(user: User): Observable<User> {
-    return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
+    return this.http.post<User>('http://app-kaua.herokuapp.com/usuarios/cadastrar', user)
   }
 
   logado() {
